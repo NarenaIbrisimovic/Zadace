21a285e20f06876bbc189551ad6ff5f050c224b2
Narena Ibrisimovic <narenaibrisimovic@Damirs-MacBook-Pro-2.local>
2015-05-26 07:19:10 +0200

    ovo je druga moja zadaca na Gitu

---
+++ Task1.java
@@ -0,0 +1,18 @@
+package zadaca5;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		int n = 500000;
+		int i = 0;
+		while (n != 0)
+		{
+			n = n / 2;
+			i++;				
+		}
+		System.out.println("Brojac:" + i);
+		
+
+	}
+
+}
