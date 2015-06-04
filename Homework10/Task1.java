3edf666a02471da06b376add47e4a144c832214b
Narena Ibrisimovic <narenaibrisimovic@Damirs-MacBook-Pro-2.local>
2015-06-04 07:38:14 +0200

    Zadaca 10

---
+++ Homework10/Task1.java
@@ -0,0 +1,22 @@
+package Homework10;
+
+import java.util.Arrays;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		int[] arr = new int[100];
+		int value = 0;
+		
+		for (int i = 0; i <arr.length; i++ ){
+			arr[i] = value;
+			value++;
+			
+			if (arr[i] == 10){
+				value = 0;
+			}
+		}
+		System.out.println(Arrays.toString(arr));
+	}
+
+}
