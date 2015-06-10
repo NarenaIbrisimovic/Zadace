4c6a7ae88f5e0cf1c3055c89ac45011db2d68e41
Narena Ibrisimovic <narenaibrisimovic@Damirs-MacBook-Pro-2.local>
2015-06-10 07:16:37 +0200

    Zadaca 13

---
+++ Homework13/Task1.java
@@ -0,0 +1,25 @@
+package Homework13;
+/*
+ * Napisati metodu koja konvertuje C u K.Parametar j double i izlaz je double. Napisati javadoc za tu metodu.
+ */
+
+public class Task1 {
+	/**
+	 * Convert Celsius to Kelvin.
+	 * @degreesCelsius input value.
+	 * @degreesKelvin - return value.
+	 */
+
+	public static void main(double[] args) {
+		System.out.println(convertToKelvins (-15.15));
+
+	}
+	
+	public static double convertToKelvins(double degreesCelsius){
+		
+		double degreesKelvin = degreesCelsius + 273.15;
+		return degreesKelvin;
+		
+	}
+
+}
